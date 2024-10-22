9ce1d63851daf028d9fd6e0932ac8d60f41ed559
Sanjay Samantra <sanjaysamantra1@gmail.com>
2024-10-22 21:39:18 +0530

    day 49

---
+++ day_49_oct_22/cron_demo/app.js
@@ -0,0 +1,38 @@
+const cron = require('node-cron');
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "sanjaysamantra2@gmail.com",
+        pass: "vqnz angb vioh jbmu", // google app password
+    }
+});
+
+let mailOptions = {
+    from: 'sanjaysamantra2@gmail.com',
+    to: ['pritampadhy@ymail.com', 'rangasravinandanahs@gmail.com', 'shindevilas1994@gmail.com'],
+    subject: "Sending Email From NodeJS",
+    html: `
+        <h1>This is a Test Email</h1>
+        <h3 style='color:red;text-align:center'>Congratulations!!</h3>
+        <p>Please follow the below instructions to claim your money</p>
+        <pre>
+            Hello
+            Hiiiiiiiiii
+            Good Morning
+        </pre>
+    `,
+    // attachments: [{ fileName: 'profile.png', path: './profile.png' }]
+}
+
+
+cron.schedule('* * * * *', () => {
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('email sent succesfully!!!')
+        }
+    })
+});
